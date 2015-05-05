e465f091e49d870fa29df126b1e6d7b1fc18fdf7
Mishal Islam <mishal.islam01@gmail.com>
2015-05-05 17:30:29 +0100

    deletes all comments if a link is destroyed

--- spec/features/comment_feature_spec.rb
+++ spec/features/comment_feature_spec.rb
@@ -1,15 +1,28 @@
 require 'rails_helper'
 
 feature 'commenting' do
-  before {Link.create title: 'something'}
 
-  scenario 'allows a user to leave a comment' do
+  before do 
+    Link.create title: 'something'
     visit '/links'
     click_link 'Comment'
     fill_in 'Thoughts', with: 'blah'
     select '3', from: 'Rating'
     click_button 'Leave comment'
+  end
+
+  scenario 'allows a user to leave a comment' do
     expect(current_path).to eq '/links'
     expect(page).to have_content 'blah, 3/5'
   end
+
+  context 'user deletes link' do
+
+    scenario 'deletes comments automatically' do
+      click_link 'Delete'
+      expect(page).not_to have_content 'blah, 3/5'
+    end
+
+  end
+
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 describe Link, type: :model do
   it { is_expected.to have_many :comments }
+
+  it 'deletes comment that belongs to the deleted link' do
+    @link = Link.create(title: 'something', id: 1)
+    @comment = Comment.create(link_id: @link.id)
+    expect(Comment.all.length).to eq 1
+    @link.destroy!
+    expect(Comment.all.length).to eq 0
+  end
 end
